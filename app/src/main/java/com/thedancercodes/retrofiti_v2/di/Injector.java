16c08d5004db75fbfb329c97b3aad78506d771c9
Roger Taracha <rojtaracha@gmail.com>
2018-10-05 06:59:12 +0300

    [FEATURE] Modifying Request URLs and Headers with OkHttp:
    [+] Application Interceptor that adds Headers and Manipulates URLs ✅

--- app/src/main/java/com/thedancercodes/retrofiti_v2/di/Injector.java
+++ app/src/main/java/com/thedancercodes/retrofiti_v2/di/Injector.java
@@ -4,7 +4,13 @@ import com.thedancercodes.retrofiti_v2.BuildConfig;
 import com.thedancercodes.retrofiti_v2.Constants;
 import com.thedancercodes.retrofiti_v2.api.SongService;
 
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -13,6 +19,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by TheDancerCodes on 04/10/2018.
  */
 public class Injector {
+
+    // Instantiate local variables
+    private static final String VERSION = "version";
+    private static final String USER_AGENT = "User-Agent";
+    private static final String RETROFITI_ANDROID_APP = "Retrofiti-Android-App";
+
 
     // Retrofit instance
     public static Retrofit provideRetrofit(String baseUrl) {
@@ -27,7 +39,27 @@ public class Injector {
     private static OkHttpClient provideOkHttpClient() {
         return new OkHttpClient.Builder()
                 .addInterceptor(provideHttpLoggingInterceptor())
+                .addInterceptor(provideUrlAndHeaderInterceptor())
                 .build();
+    }
+
+    // Application Interceptor that adds Headers and Manipulates URLs
+    private static Interceptor provideUrlAndHeaderInterceptor() {
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                HttpUrl url = request.url()
+                        .newBuilder()
+                        .addQueryParameter(VERSION, BuildConfig.VERSION_NAME)
+                        .build();
+
+                Request.Builder builder = request.newBuilder().url(url);
+                builder.addHeader(USER_AGENT, RETROFITI_ANDROID_APP);
+
+                return chain.proceed(builder.build());
+            }
+        };
     }
 
     // HttpLoggingInterceptor:
